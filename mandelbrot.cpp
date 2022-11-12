439e2a87453660542d0b73734db7b9636c977a09
Leskodamus <florian@leskovsek.dev>
2022-11-12 13:36:41 +0100

    Add Area and Scale structs
    Making the entirety more OOP and better structured

--- mandelbrot.cpp
+++ mandelbrot.cpp
@@ -4,13 +4,36 @@
 #include <opencv4/opencv2/core.hpp>
 #include <opencv4/opencv2/imgcodecs.hpp>
 
-using namespace std;
 using namespace cv;
 
-/**
- * formula iteration: z(n+1) = z(n)^2 + c
-*/
+/* Area domain for points */
+struct Area {
+    double x_min;
+    double x_max;
+    double y_min;
+    double y_max;
 
+    Area(double _x_min, double _x_max, double _y_min, double _y_max) :
+        x_min(_x_min), x_max(_x_max),
+        y_min(_y_min), y_max(_y_max) {}
+};
+
+/* Scale for Area to Mat */
+struct Scale {
+    double x;
+    double y;
+    
+    Scale() = default;
+
+    Scale(Mat &img, Area &area) :
+        x(img.cols / (area.x_max - area.x_min)), 
+        y(img.rows / (area.y_max - area.y_min)) {}
+};
+
+/**
+ * Escape Time Algorithm
+ * formula: z(n+1) = z(n)^2 + c
+*/
 int escape_time(const std::complex<double> &c, const int limit) {
     std::complex<double> z = c;
     for (int i = 0; i < limit; ++i) {
@@ -20,20 +43,23 @@ int escape_time(const std::complex<double> &c, const int limit) {
     return limit;
 }
 
-int mandelbrot_greyscale(std::complex<double> &c, const int limit = 200) {
+/* Get greyscale value 0-255 */
+int mandelbrot_greyscale(const std::complex<double> &c, const int limit = 200) {
     int value = escape_time(c, limit);
     if (limit - value == 0) return 0;
     return cvRound(sqrt(value / static_cast<double>(limit)) * 255);
 }
 
-void mandelbrot(Mat &img, const double x1, const double y1,
-                const double scaleX, const double scaleY) 
-{
+/**
+ * Sequentially generate mandelbrot set image
+*/
+void mandelbrot(Mat &img, Area &area) {
+    Scale scale(img, area);
     for (int i = 0; i < img.rows; ++i) {
         for (int j = 0; j < img.cols; ++j) {
-            double x0 = j / scaleX + x1;
-            double y0 = i / scaleY + y1;
-            std::complex<double> c(x0, y0);
+            double real = j / scale.x + area.x_min;
+            double imag = i / scale.y + area.y_min;
+            std::complex<double> c(real, imag);
             int value = mandelbrot_greyscale(c);
             img.ptr<uchar>(i)[j] = static_cast<uchar>(value);
         }
@@ -42,14 +68,9 @@ void mandelbrot(Mat &img, const double x1, const double y1,
 
 int main(void) {
     Mat img(400, 600, CV_8U);
-    double x1 = -2.1, x2 = 0.6;
-    double y1 = -1.2, y2 = 1.2;
-    double scaleX = img.cols / (x2 - x1);
-    double scaleY = img.rows / (y2 - y1);
-
-    mandelbrot(img, x1, y1, scaleX, scaleY);
+    Area area(-2.1, 0.6, -1.2, 1.2);
+    mandelbrot(img, area);
     imwrite("mandelbrot.png", img);
-
     return 0;
 }
 
